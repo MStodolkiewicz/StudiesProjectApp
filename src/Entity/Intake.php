fb2ea9a41d7653cf90442fb35c438eeb974eb6de
Swicku <pacu157@gmail.com>
2021-10-26 17:05:23 +0200

    entities without relations
    fast commit for adding entities that has been left without adding, atm without relations with each other, look up next commit.

---
+++ src/Entity/Intake.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\IntakeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=IntakeRepository::class)
+ */
+class Intake
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="decimal", precision=6, scale=2)
+     */
+    private $amountInGrams;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmountInGrams(): ?string
+    {
+        return $this->amountInGrams;
+    }
+
+    public function setAmountInGrams(string $amountInGrams): self
+    {
+        $this->amountInGrams = $amountInGrams;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Rate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\RateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=RateRepository::class)
+ */
+class Rate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
