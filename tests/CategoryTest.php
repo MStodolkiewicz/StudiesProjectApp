028207b7b52067df9aa019834ff0b94d603f5119
Swicku <pacu157@gmail.com>
2021-11-03 21:49:52 +0100

    testes, added some fixed dummy data for tests performing

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -1,5 +1,5 @@
 <?php
-// api/tests/BooksTest.php
+#tests/CategoryTest.php
 
 namespace App\Tests;
 
@@ -16,7 +16,7 @@ class CategoryTest extends ApiTestCase
     public function testGetCollection(): void
     {
         // The client implements Symfony HttpClient's `HttpClientInterface`, and the response `ResponseInterface`
-        $response = static::createClient()->request('GET', '/books');
+        $response = static::createClient()->request('GET', '/api/categories');
 
         $this->assertResponseIsSuccessful();
         // Asserts that the returned content type is JSON-LD (the default)
@@ -24,115 +24,94 @@ class CategoryTest extends ApiTestCase
 
         // Asserts that the returned JSON is a superset of this one
         $this->assertJsonContains([
-            '@context' => '/contexts/Book',
-            '@id' => '/books',
+            '@context' => "/api/contexts/Category",
+            '@id' => "/api/categories",
             '@type' => 'hydra:Collection',
-            'hydra:totalItems' => 100,
+            "hydra:totalItems" => 51,
             'hydra:view' => [
-                '@id' => '/books?page=1',
-                '@type' => 'hydra:PartialCollectionView',
-                'hydra:first' => '/books?page=1',
-                'hydra:last' => '/books?page=4',
-                'hydra:next' => '/books?page=2',
+                "@id" => "/api/categories?page=1",
+                "@type" => "hydra:PartialCollectionView",
+                "hydra:first" => "/api/categories?page=1",
+                "hydra:last" => "/api/categories?page=51",
+                "hydra:next" => "/api/categories?page=2",
             ],
         ]);
 
         // Because test fixtures are automatically loaded between each test, you can assert on them
-        $this->assertCount(30, $response->toArray()['hydra:member']);
+        $this->assertCount(1, $response->toArray()['hydra:member']);
 
         // Asserts that the returned JSON is validated by the JSON Schema generated for this resource by API Platform
         // This generated JSON Schema is also used in the OpenAPI spec!
-        $this->assertMatchesResourceCollectionJsonSchema(Book::class);
+        $this->assertMatchesResourceCollectionJsonSchema(Category::class);
     }
 
-    public function testCreateBook(): void
+    public function testCreateCategory(): void
     {
-        $response = static::createClient()->request('POST', '/books', ['json' => [
-            'isbn' => '0099740915',
-            'title' => 'The Handmaid\'s Tale',
-            'description' => 'Brilliantly conceived and executed, this powerful evocation of twenty-first century America gives full rein to Margaret Atwood\'s devastating irony, wit and astute perception.',
-            'author' => 'Margaret Atwood',
-            'publicationDate' => '1985-07-31T00:00:00+00:00',
+        $response = static::createClient()->request('POST', '/api/categories', ['json' => [
+            'name' => 'Fruits'
         ]]);
 
         $this->assertResponseStatusCodeSame(201);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
         $this->assertJsonContains([
-            '@context' => '/contexts/Book',
-            '@type' => 'Book',
-            'isbn' => '0099740915',
-            'title' => 'The Handmaid\'s Tale',
-            'description' => 'Brilliantly conceived and executed, this powerful evocation of twenty-first century America gives full rein to Margaret Atwood\'s devastating irony, wit and astute perception.',
-            'author' => 'Margaret Atwood',
-            'publicationDate' => '1985-07-31T00:00:00+00:00',
-            'reviews' => [],
+            '@context' => '/api/contexts/Category',
+            '@type' => 'Category',
+            'name' => 'Fruits',
+            'products' => [],
+            'subCategories' => [],
         ]);
-        $this->assertMatchesRegularExpression('~^/books/\d+$~', $response->toArray()['@id']);
-        $this->assertMatchesResourceItemJsonSchema(Book::class);
+        $this->assertMatchesRegularExpression('/\/api\/categories\/*/', $response->toArray()['@id']);
+        $this->assertMatchesResourceItemJsonSchema(Category::class);
     }
 
-    public function testCreateInvalidBook(): void
+    public function testCreateInvalidCategory(): void
     {
-        static::createClient()->request('POST', '/books', ['json' => [
-            'isbn' => 'invalid',
+        $response = static::createClient()->request('POST', '/api/categories', ['json' => [
+            'name' => 123
         ]]);
 
-        $this->assertResponseStatusCodeSame(422);
+        $this->assertResponseStatusCodeSame(400);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
 
         $this->assertJsonContains([
-            '@context' => '/contexts/ConstraintViolationList',
-            '@type' => 'ConstraintViolationList',
-            'hydra:title' => 'An error occurred',
-            'hydra:description' => 'isbn: This value is neither a valid ISBN-10 nor a valid ISBN-13.
-title: This value should not be blank.
-description: This value should not be blank.
-author: This value should not be blank.
-publicationDate: This value should not be null.',
+            "@context" => "/api/contexts/Error",
+            "@type" => "hydra:Error",
+            "hydra:title" => "An error occurred",
+            "hydra:description" => "The type of the \"name\" attribute must be \"string\", \"integer\" given.",
         ]);
     }
 
-    public function testUpdateBook(): void
+    public function testUpdateCategory(): void
     {
         $client = static::createClient();
+
         // findIriBy allows to retrieve the IRI of an item by searching for some of its properties.
         // ISBN 9786644879585 has been generated by Alice when loading test fixtures.
         // Because Alice use a seeded pseudo-random number generator, we're sure that this ISBN will always be generated.
-        $iri = $this->findIriBy(Book::class, ['isbn' => '9781344037075']);
+        $iri = $this->findIriBy(Category::class, ['name' => 'Mushrooms']);
 
         $client->request('PUT', $iri, ['json' => [
-            'title' => 'updated title',
+            'name' => 'Vegetables'
         ]]);
 
         $this->assertResponseIsSuccessful();
         $this->assertJsonContains([
             '@id' => $iri,
-            'isbn' => '9781344037075',
-            'title' => 'updated title',
+            'name' => 'Vegetables',
         ]);
     }
 
-    public function testDeleteBook(): void
+    public function testDeleteCategoryAssignedToProduct(): void
     {
         $client = static::createClient();
-        $iri = $this->findIriBy(Book::class, ['isbn' => '9781344037075']);
+        $categoryRepository = $this->getContainer()->get('doctrine')->getRepository(Category::class);
+        $iri = $this->findIriBy(Category::class, ['name' => 'Mushrooms']);
 
         $client->request('DELETE', $iri);
+        $this->assertResponseStatusCodeSame(500);
+        $category = $categoryRepository->findOneBy(['name' => 'Mushrooms']);
+        $this->assertNotNull($category);
 
-        $this->assertResponseStatusCodeSame(204);
-        $this->assertNull(
-        // Through the container, you can access all your services from the tests, including the ORM, the mailer, remote API clients...
-            static::$container->get('doctrine')->getRepository(Book::class)->findOneBy(['isbn' => '9781344037075'])
-        );
-    }
 
-    public function testLogin(): void
-    {
-        $response = static::createClient()->request('POST', '/login', ['json' => [
-            'email' => 'admin@admin.pl',
-            'password' => 'admin',
-        ]]);
-
-        $this->assertResponseIsSuccessful();
     }
 }
